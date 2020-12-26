c4f1b7e890e57ad143f86c5cb20f705366060eb7
aditgocendra <gocendra123@gmail.com>
2020-12-26 12:23:32 +0800

    change fire property and label main menu

---
+++ src/Object/FireCPUParticles.gd
@@ -0,0 +1,17 @@
+extends CPUParticles
+
+var timer
+
+func _ready():
+	randomize()
+	
+	timer = Timer.new()
+	timer.wait_time = rand_range(0.05, 0.1)
+	timer.connect("timeout", self, "on_timer_timeout")
+	add_child(timer)
+	timer.start()
+	
+	
+func on_timer_timeout():
+	timer.wait_time = rand_range(0.05, 0.1)
+	$OmniLight.light_energy = rand_range(0.0 , 1.0)
